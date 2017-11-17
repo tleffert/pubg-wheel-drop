051457d84d047fa009e258b151cf3b29218e0176
Trevor L <leffertt@gmail.com>
2017-11-16 20:44:42 -0600

    Moving Server.js to Server folder for build/bundle

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // Set our api routes
 // app.use('/api', api);
