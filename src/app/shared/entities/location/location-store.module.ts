cd3200c6d5fccb25dd57c3bf5f6ff7f92a8a1a92
Trevor Leffert <leffertt@gmail.com>
2019-09-14 16:46:42 -0500

    Added hydration metareducers for map snad locations

--- src/app/shared/entities/location/location-store.module.ts
+++ src/app/shared/entities/location/location-store.module.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+
+import { StoreModule, ActionReducer, MetaReducer } from '@ngrx/store';
+import { EffectsModule, ROOT_EFFECTS_INIT } from '@ngrx/effects';
+
+import { LocalStorageService } from '@app/localStorageService';
+
 import { LocationEffects } from './location-effects';
 import { locationEntityCollectionReducer } from './location-reducer';
+import { LocationEntityCollectionState } from './location-state';
+import { LocationActionKey } from './location-actions';
+
+import { hydrateState, storeStateSlice } from '../../services/localStorageMetaReducers';
+
+export const metaReducers: MetaReducer<any>[] = [storeStateSlice(LocationActionKey), hydrateState(LocationActionKey)];
 
 @NgModule({
     imports: [
         CommonModule,
-        StoreModule.forFeature('locations', locationEntityCollectionReducer),
+        StoreModule.forFeature('locations', locationEntityCollectionReducer, { metaReducers }),
         EffectsModule.forFeature([LocationEffects])
     ],
     providers: []
--- src/app/shared/entities/map/map-store.module.ts
+++ src/app/shared/entities/map/map-store.module.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+
+import { StoreModule, ActionReducer, MetaReducer } from '@ngrx/store';
+import { EffectsModule, ROOT_EFFECTS_INIT } from '@ngrx/effects';
+
+import { LocalStorageService } from '@app/localStorageService';
+
 import { MapEffects } from './map-effects';
 import { mapEntityCollectionReducer } from './map-reducer';
+import { MapEntityCollectionState } from './map-state';
+import { MapActionKey } from './map-actions';
+
+import { hydrateState, storeStateSlice } from '../../services/localStorageMetaReducers';
+
+export const metaReducers: MetaReducer<any>[] = [storeStateSlice(MapActionKey), hydrateState(MapActionKey)];
 
 @NgModule({
     imports: [
         CommonModule,
-        StoreModule.forFeature('maps', mapEntityCollectionReducer),
+        StoreModule.forFeature('maps', mapEntityCollectionReducer, { metaReducers }),
         EffectsModule.forFeature([MapEffects])
     ],
     providers: []
