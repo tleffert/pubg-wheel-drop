447a5df89a6d9c2f2d75666af4ae6ba0666f5c12
Trevor Leffert <leffertt@gmail.com>
2019-09-14 16:50:16 -0500

    cleanup

--- src/app/shared/entities/map/map-actions.ts
+++ src/app/shared/entities/map/map-actions.ts
@@ -1,15 +1,8 @@
 import { createAction, props} from '@ngrx/store';
 
 import { Map } from '@app/types';
-import { MapEntity } from './map-state';
 
 export const MapActionKey = '[Map]';
-
-export enum Events {
-    FETCH_ALL_MAPS = '[Map] FETCH_ALL_MAPS',
-    FETCH_ALL_MAPS_SUCCESS = '[Map] FETCH_ALL_MAPS_SUCCESS',
-    SELECT_MAP = '[Map] SELECT_MAP',
-}
 
 export const fetchAllMaps = createAction(
     `${MapActionKey} FETCH_ALL_MAPS`
