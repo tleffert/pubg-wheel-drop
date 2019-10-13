89e3358c4d58d5d17b99504fdb3afd50fcd1172d
Trevor Leffert <leffertt@gmail.com>
2019-10-13 16:24:27 -0500

    Added location group component, with dynamic template

---
+++ src/app/location-selection/location-group/location-group.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, TemplateRef, ContentChild, AfterContentInit } from '@angular/core';
+
+import { Location } from '@app/types';
+
+import { LocationGroupToggle } from '../location-group-toggle.directive';
+
+export interface SelectableLocation {
+    location: Location;
+    selected: boolean;
+}
+
+@Component({
+  selector: 'app-location-group',
+  templateUrl: './location-group.component.html',
+  styleUrls: ['./location-group.component.scss']
+})
+export class LocationGroupComponent implements OnInit {
+
+    @Input()
+    locations: Location[] = [];
+
+    @Output()
+    selectedLocation = new EventEmitter<SelectableLocation>();
+
+    @ContentChild(LocationGroupToggle, {read: TemplateRef, static: false})
+    groupToggleTemplate: TemplateRef<any>;
+
+    allSelected: boolean;
+
+    numSelected: number;
+
+    locationMap: Map<string, SelectableLocation> = new Map();
+
+    constructor() {}
+
+    ngOnInit() {
+        this.locations.forEach(location => {
+            this.locationMap.set(location._id, {
+                location: location,
+                selected: location.selected
+            });
+        });
+    }
+
+    selectedUpate(selected: boolean, location: Location) {
+        this.locationMap.get(location._id).selected = selected;
+        console.log(this.locationMap.get(location._id));
+        this.selectedLocation.emit(this.locationMap.get(location._id));
+    }
+
+}
