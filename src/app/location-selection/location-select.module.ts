a93a2749a4f80d3adf7cac86b30863ffa74c1eff
Trevor Leffert <leffertt@gmail.com>
2019-10-13 16:25:51 -0500

    Updated location-select component to use new directives/components for location select

--- src/app/location-selection/location-select.module.ts
+++ src/app/location-selection/location-select.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { LocationSelectComponent } from './location-select.component';
 import { CommonModule } from '@angular/common';
+import { LocationOption } from './location-option.component';
+import { LocationGroupComponent } from './location-group/location-group.component';
+import { LocationGroupToggle } from './location-group-toggle.directive';
 
 @NgModule({
     imports : [CommonModule],
-    declarations: [ LocationSelectComponent ],
-    exports : [LocationSelectComponent]
+    declarations: [ LocationSelectComponent, LocationOption, LocationGroupComponent,
+        LocationGroupToggle
+    ],
+    exports : [LocationSelectComponent, LocationOption, LocationGroupComponent, LocationGroupToggle]
 })
 export class LocationSelectModule { }
